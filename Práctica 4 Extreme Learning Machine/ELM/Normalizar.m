695fc8149f4fc49b9ead7b3232fe0cfff2c66d3e
Isi-17 <isidrojova@uma.es>
2023-11-22 20:36:31 +0100

    Fix

--- Práctica 4 Extreme Learning Machine/ELM/Normalizar.m
+++
@@ -1,9 +0,0 @@
-function [Label] = Normalizar(A)
-%Normalizar Summary of this function goes here
-%   Detailed explanation goes here
-[M, I] = max(A');
-        Label = zeros(size(A));
-        for k = 1:size(A,1)
-            Label(k, I(k)) = 1;
-        end
-end
